6ff61c56f07ae76def1ca0de7f9c2056736d27ce
Sudipta jana <sudiptajana70@gmail.com>
2023-07-24 13:00:08 +0530

    established connection with database

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const identifyRoute = require('./routes/identifyRoutes')
 app.use('/api', identifyRoute)
 
 
+// Database connection
+const dbConnection = require("./config/db");
+
+
 // Start the server
 app.listen(process.env.PORT || 5000 , () => {
+  dbConnection.authenticate();
+  console.log("Db connected successfully")
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
